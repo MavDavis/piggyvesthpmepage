e2682831957760737d4619804d1a0d5a68475f47
MavDavis <ekwesimarvelous@gmail.com>
2022-03-26 18:20:12 +0100

    demo comit

---
+++ index.js
@@ -0,0 +1,15 @@
+  const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function(e){
+ 
+  let navbar = document.querySelector('.navLink')
+  if(navbar.style.display === 'none'){
+    navbar.style.display =' block' ;}
+  else{
+    navbar.style.display =' none' 
+  };
+  
+  if(){}
+  else{
+
+  }
+})
