25bbbce6e1a75216a340aefcd7a77cebf455ce94
MavDavis <ekwesimarvelous@gmail.com>
2022-03-27 14:44:04 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+  
+  function navbar(){
+
+    let navList = document.getElementsByClassName('nav-links');
+    for(var i = 0; i < navList.length; i++){
+      navList[i].addEventListener('click', function(e){
+        e.preventDefault();
+        let navLink = document.querySelector('.navLink').style.display = 'none';
+        let signButton = document.querySelector('.sign').style.display = 'none';
+        let nav = document.querySelector('.nav')
+nav.classList.toggle('show-div');
+      })
+    }
+    }
+  
   const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function(e){
- 
-  let navbar = document.querySelector('.navLink')
-  if(navbar.style.display === 'none'){
-    navbar.style.display =' block' ;}
-  else{
-    navbar.style.display =' none' 
-  };
-  
-  if(){}
-  else{
+let nav = document.querySelector('.nav')
+nav.classList.toggle('show-div');
 
+  let navLink = document.querySelector('.navLink');
+   let signButton = document.querySelector('.sign');
+ 
+  if(navLink.style.display === 'block' && signButton.style.display === 'block'){
+    navLink.style.display = 'none' ;
+    signButton.style.display = 'none'
   }
+  else{
+    navLink.style.display =' block';
+    signButton.style.display = 'block'
+  };
+navbar();
 })
